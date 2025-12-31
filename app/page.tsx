3ff39bfe833cb05c774196789348041376427714
IlyaG <ilyag@MacBook-Air-M4--G.local>
2025-12-31 21:27:14 +0500

    main5final

--- app/page.tsx
+++ app/page.tsx
@@ -351,7 +351,7 @@ export default function Home() {
             zIndex: 9999,
             boxShadow: '0 4px 12px rgba(0,0,0,0.2)'
           }}>
-            Copied!
+            C9FeqSCd2praTksKP2jX56MDqttjSmxSFKsnvAA8pump
           </div>
 
           <div style={{
